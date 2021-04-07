ea4c508f9aeb7f03e1ac75e22491d8e6fa21bf84
zpskek <wjdghdwns0@gmail.com>
2021-04-07 15:56:21 +0900

    Create header of restaurant-detail

---
+++ src/pages/client/restaurant.tsx
@@ -0,0 +1,67 @@
+import { gql, useQuery } from "@apollo/client";
+import React from "react";
+import { Helmet } from "react-helmet-async";
+import { useParams } from "react-router-dom";
+import { RESTAURANT_FRAGMENT } from "../../fragments";
+import {
+  restaurant,
+  restaurantVariables,
+} from "../../__generated__/restaurant";
+
+const RESTAURANT_QUERY = gql`
+  query restaurant($input: RestaurantInput!) {
+    restaurant(input: $input) {
+      ok
+      error
+      restaurant {
+        ...RestaurantParts
+      }
+    }
+  }
+  ${RESTAURANT_FRAGMENT}
+`;
+
+interface IRestaurantParams {
+  id: string;
+}
+
+// react-router-dom은 총 3가지 hook을 제공한다.
+// useLocation은 우리가 어디있는지 url을 알게 해준다.
+// useHistory는 change, replace, push를 통해 이곳 저곳 돌아다닐 수 있게 해준다.
+// useParams는 우리에게 parameter를 준다.
+export const Restaurant = () => {
+  const params = useParams<IRestaurantParams>();
+  const { loading, data } = useQuery<restaurant, restaurantVariables>(
+    RESTAURANT_QUERY,
+    {
+      variables: {
+        input: {
+          restaurantId: +params.id,
+        },
+      },
+    }
+  );
+  return (
+    <div>
+      <Helmet>
+        <title>{data?.restaurant.restaurant?.name || ""} | Bixby Eats</title>
+      </Helmet>
+      <div
+        className=" bg-gray-800 bg-center bg-cover py-48"
+        style={{
+          backgroundImage: `url(${data?.restaurant.restaurant?.coverImg})`,
+        }}
+      >
+        <div className="bg-white w-3/12 py-8 pl-48">
+          <h4 className="text-4xl mb-3">{data?.restaurant.restaurant?.name}</h4>
+          <h5 className="text-sm font-light mb-2">
+            {data?.restaurant.restaurant?.category?.name}
+          </h5>
+          <h6 className="text-sm font-light">
+            {data?.restaurant.restaurant?.address}
+          </h6>
+        </div>
+      </div>
+    </div>
+  );
+};
