f0521e0b4268bf1df28dd031ca5f86cab1e02df8
zpskek <wjdghdwns0@gmail.com>
2021-05-03 15:09:13 +0900

    Delete verify email message on the header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -18,11 +18,6 @@ export const Header: React.FC = () => {
   };
   return (
     <>
-      {!data?.me.verified && (
-        <div className="bg-red-500 p-3 text-center text-base text-white">
-          <span>Please verify your email.</span>
-        </div>
-      )}
       <header className="py-4">
         <div className="w-full px-5 xl:px-0 max-w-screen-xl mx-auto flex justify-between items-center">
           <Link to="/">
