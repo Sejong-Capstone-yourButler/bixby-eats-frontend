35f6bbd160d553cb063e8221f1eda431d81448ae
zpskek <wjdghdwns0@gmail.com>
2021-05-26 21:04:47 +0900

    Translate from english to korean on 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,14 +5,14 @@ import { Link } from "react-router-dom";
 export const NotFound = () => (
   <div className="h-screen flex flex-col items-center justify-center">
     <Helmet>
-      <title>Not Found | Bixby Eats</title>
+      <title>Not Found | 쿠게더</title>
     </Helmet>
-    <h2 className="font-semibold text-2xl mb-3">Page Not Found.</h2>
+    <h2 className="font-semibold text-2xl mb-3">페이지를 찾을 수 없습니다.</h2>
     <h4 className="font-medium text-base mb-5">
-      The page you're looking for does not exist or has moved.
+      찾으시는 페이지가 존재하지 않거나 다른 곳으로 이동하였습니다.
     </h4>
     <Link className="hover:underline text-lime-600" to="/">
-      Go back home &rarr;
+      홈으로 돌아가기 &rarr;
     </Link>
   </div>
 );
